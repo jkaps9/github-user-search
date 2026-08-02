f8827725637fba306b44697f660600b6da3d10e5
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-02 17:29:26 -0400

    add search bar to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
-
+import SearchBar from "./components/SearchBar";
 export default function App() {
   return (
     <>
       <Header></Header>
+      <main>
+        <SearchBar></SearchBar>
+      </main>
     </>
   );
 }
