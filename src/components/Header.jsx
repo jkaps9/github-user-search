41e8d47fc86d96e6e6df85634ef10690f045a838
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-02 17:26:44 -0400

    add logo to header and add padding to body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import ThemeToggle from "./ThemeToggle";
 export default function Header() {
   return (
     <header>
-      <div className="container">
+      <div className="container row">
+        <h1>devFinder</h1>
         <ThemeToggle></ThemeToggle>
       </div>
     </header>
