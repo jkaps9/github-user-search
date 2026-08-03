705456710ad6dc5259c5336f8faec1f1a94d6372
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-02 21:56:55 -0400

    add error message

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,10 @@
 import styles from "./SearchBar.module.css";
+import searchIcon from "../assets/icons/icon-search.svg";
 
 export default function SearchBar() {
   return (
     <form className={`${styles.searchBar} row`}>
+      <img src={searchIcon} alt="" aria-hidden="true" />
       <label htmlFor="search" className="sr-only">
         Search
       </label>
@@ -11,7 +13,11 @@ export default function SearchBar() {
         id="search"
         name="search"
         placeholder="Search GitHub username..."
+        aria-describedby="search-error"
       />
+      <p id="search-error" className={styles.searchError}>
+        No results
+      </p>
       <button className="btn btn--solid" type="submit">
         Search
       </button>
