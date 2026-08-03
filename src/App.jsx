e30e01fcb1f7694f477f1db2ab993b381463776d
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-03 00:01:42 -0400

    add data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import { useState } from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import Profile from "./components/Profile";
+import { useEffect } from "react";
+
+const BASE_URL = "https://api.github.com/users";
 
 export default function App() {
+  const [data, setData] = useState([]);
   const [currentUser, setCurrentUser] = useState("octocat");
 
   const updateUser = (newUser) => {
     setCurrentUser(newUser);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/${currentUser}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [currentUser]);
   return (
     <>
       <Header></Header>
@@ -18,6 +39,7 @@ export default function App() {
       </div>
       <div className="container">
         <Profile></Profile>
+        <div>{JSON.stringify(data)}</div>
       </div>
     </>
   );
