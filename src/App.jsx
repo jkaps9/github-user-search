9aac409eee28199768514a06a73b59b32b0f148f
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-02 22:24:36 -0400

    create profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
+import Profile from "./components/Profile";
 export default function App() {
   return (
     <>
@@ -7,6 +8,7 @@ export default function App() {
       <main>
         <div className="container">
           <SearchBar></SearchBar>
+          <Profile></Profile>
         </div>
       </main>
     </>
